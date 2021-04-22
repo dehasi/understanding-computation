a1aadfc4d20635c3c5104f572c240d3c4ae683a1
Ravil Galeyev <dehasi@yandex.ru>
2021-04-22 22:50:13 +0500

    Add classes

--- test/ch2_test.rb
+++ test/ch2_test.rb
@@ -6,4 +6,12 @@ class Ch2Test < Test::Unit::TestCase
   def test_number
     assert Number.new(42).value == 42
   end
+
+  def test_build_expresstion
+    exp = Add.new(
+      Multiply.new(Number.new(1), Number.new(2)),
+      Multiply.new(Number.new(3), Number.new(4)))
+
+    puts exp
+  end
 end
