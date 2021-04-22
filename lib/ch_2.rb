878d26ca4b8f5615f03eb2cca1a26bab75bfdcec
Ravil Galeyev <dehasi@yandex.ru>
2021-04-23 01:00:46 +0500

    Add variable

--- lib/ch_2.rb
+++ lib/ch_2.rb
@@ -102,6 +102,25 @@ class LessThan < Struct.new(:left, :right)
   end
 end
 
+class Variable < Struct.new(:name)
+
+  def reduce(environment)
+    environment[name]
+  end
+
+  def reducible?
+    true
+  end
+
+  def to_s
+    name.to_s
+  end
+
+  def inspect
+    "\"#{self}\""
+  end
+end
+
 class Machine < Struct.new(:expression)
 
   def step
