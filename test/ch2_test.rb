e76339a12d82401eabc2623880b5cb64ce3bdab7
Ravil Galeyev <dehasi@yandex.ru>
2021-04-22 22:54:09 +0500

    Add to s

--- test/ch2_test.rb
+++ test/ch2_test.rb
@@ -7,11 +7,11 @@ class Ch2Test < Test::Unit::TestCase
     assert Number.new(42).value == 42
   end
 
-  def test_build_expresstion
+  def test_build_expression
     exp = Add.new(
       Multiply.new(Number.new(1), Number.new(2)),
       Multiply.new(Number.new(3), Number.new(4)))
 
-    puts exp
+    assert_equal exp.to_s, '1 * 2 + 3 * 4'
   end
 end
