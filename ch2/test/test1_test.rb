76d95f65fdb2f4f1ad7a6d1acd128def1da898d5
Ravil Galeyev <dehasi@yandex.ru>
2021-04-22 20:01:14 +0500

    Copy-paste rake

---
+++ ch2/test/test1_test.rb
@@ -0,0 +1,19 @@
+require 'test/unit'
+
+class Test1Test < Test::Unit::TestCase
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    skip 'Not implemented'
+  end
+
+  def test_2
+    assert true
+  end
+end
