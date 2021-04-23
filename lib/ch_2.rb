dfebd8e5f5abc068947104b457b0ea68e54d70b5
Ravil Galeyev <dehasi@yandex.ru>
2021-04-23 05:53:54 +0500

    Add if

--- lib/ch_2.rb
+++ lib/ch_2.rb
@@ -163,11 +163,39 @@ class Assign < Struct.new(:name, :expression)
   end
 end
 
+class If < Struct.new(:condition, :consequence, :alternative)
+
+  def reduce(environment)
+    if condition.reducible?
+      [If.new(condition.reduce(environment), consequence, alternative), environment]
+    else
+      case condition
+      when Boolean.new(true)
+        [consequence, environment]
+      when Boolean.new(false)
+        [alternative, environment]
+      end
+    end
+  end
+
+  def reducible?
+    true
+  end
+
+  def to_s
+    "if (#{condition}) { #{consequence} } else { #{alternative} }"
+  end
+
+  def inspect
+    "\"#{self}\""
+  end
+end
+
 class Machine < Struct.new(:statement, :environment)
 
   def step
     # puts "DEBUG: #{statement}, #{environment}, #{statement.class}"
-    if statement.instance_of?(Assign)
+    if statement.instance_of?(Assign) or statement.instance_of?(If)
       self.statement, self.environment = statement.reduce(environment)
     else
       self.statement = statement.reduce(environment)
--- test/ch2_test.rb
+++ test/ch2_test.rb
@@ -46,6 +46,17 @@ class Ch2Test < Test::Unit::TestCase
       Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))), { x: Number.new(2) }
     ).run
 
-    assert  result.instance_of? DoNothing
+    assert result.instance_of? DoNothing
+  end
+
+  def test_if
+    result = Machine.new(If.new(
+      Variable.new(:x),
+      Assign.new(:y, Number.new(1)),
+      Assign.new(:y, Number.new(2))
+    ), { x: Boolean.new(true) }
+    ).run
+
+    assert result.instance_of? DoNothing
   end
 end
