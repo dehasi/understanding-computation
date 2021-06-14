ed82df3b221e8ce41ae00cfd3de9629f6f91a395
Ravil Galeyev <dehasi@yandex.ru>
2021-06-14 22:33:13 +0200

    Variable

--- lib/ch_2_big_step_semantics.rb
+++ lib/ch_2_big_step_semantics.rb
@@ -1,1 +1,59 @@
+class Number < Struct.new(:value)
+  def evaluate(environment)
+    self
+  end
 
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "\"#{self}\""
+  end
+end
+
+class Boolean < Struct.new(:value)
+  def evaluate(environment)
+    self
+  end
+
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "\"#{self}\""
+  end
+end
+
+class Variable < Struct.new(:name)
+  def evaluate(environment)
+    environment[name]
+  end
+
+  def to_s
+    name.to_s
+  end
+
+  def inspect
+    "\"#{self}\""
+  end
+end
+
+class Add
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value + right.evaluate(environment).value)
+  end
+end
+
+class Multiply
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value * right.evaluate(environment).value)
+  end
+end
+
+class LessThab
+  def evaluate(environment)
+    Boolean.new(left.evaluate(environment).value < right.evaluate(environment).value)
+  end
+end
--- test/ch_2_big_step_semantics_test.rb
+++ test/ch_2_big_step_semantics_test.rb
@@ -2,4 +2,18 @@ require 'test/unit'
 require 'ch_2_big_step_semantics'
 
 class Ch2BigStepSemanticsTest < Test::Unit::TestCase
+
+  def test_number
+    number_new = Number.new(23)
+    p number_new
+    p number_new.evaluate({})
+    assert number_new.evaluate({}).to_s == "23"
+  end
+
+  def test_variable
+    number_new = Number.new(23)
+    p number_new
+    p number_new.evaluate({})
+    assert number_new.evaluate({}).to_s == "23"
+  end
 end
