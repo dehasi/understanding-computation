6e9df7fdcdbeef1d36cee296c5581776d59f0f25
Ravil Galeyev <dehasi@yandex.ru>
2021-06-14 22:35:07 +0200

    Variable

--- test/ch_2_big_step_semantics_test.rb
+++ test/ch_2_big_step_semantics_test.rb
@@ -4,16 +4,10 @@ require 'ch_2_big_step_semantics'
 class Ch2BigStepSemanticsTest < Test::Unit::TestCase
 
   def test_number
-    number_new = Number.new(23)
-    p number_new
-    p number_new.evaluate({})
-    assert number_new.evaluate({}).to_s == "23"
+    assert Number.new(23).evaluate({}).to_s == "23"
   end
 
   def test_variable
-    number_new = Number.new(23)
-    p number_new
-    p number_new.evaluate({})
-    assert number_new.evaluate({}).to_s == "23"
+    assert Variable.new(:x).evaluate({ x: Number.new(23) }).to_s == "23"
   end
 end
